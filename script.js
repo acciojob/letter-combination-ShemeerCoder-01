3fbc6f3795c40cf336b1cbfe64285f9f0757c0ed
ShemeerCoder-01 <118423467+ShemeerCoder-01@users.noreply.github.com>
2023-03-20 12:06:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
+var combination = {0 : '0', 1 : '1', 2 : 'abc', 3 : 'def', 4 : 'ghi', 5 : 'jkl', 6 : 'mno', 7 : 'pqrs', 8 : 'tuv', 9 : 'wxyz'};
+
 function letterCombinations(input_digit) {
   //Complete the function
+	var combination = {0 : '0', 1 : '1', 2 : 'abc', 3 : 'def', 4 : 'ghi', 5 : 'jkl', 6 : 'mno', 7 : 'pqrs', 8 : 'tuv', 9 : 'wxyz'};
+	
+	
+	
 }
 
 module.exports = letterCombinations;
